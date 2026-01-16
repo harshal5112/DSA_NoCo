34253be938b3bb7a6b0584e13b2b0f7aeaca2ce7
harshal5112 <harshalrasoda@gmail.com>
2026-01-16 15:41:09 +0530

    StarPatternReverse

--- src/main/java/org/example/Patterns_p1/ReversePattern.java
+++ src/main/java/org/example/Patterns_p1/ReversePattern.java
@@ -1,18 +1,26 @@
 package org.example.Patterns_p1;
 
 import java.util.Scanner;
+import java.util.logging.SocketHandler;
 
 public class ReversePattern {
     public static void RevStar(int n) {
 
-        for (int line = 1; line<=n; line++ ){
-            for (int star=1;star<=n-line+1;star++){
+//        for (int line = 1; line<=n; line++ ){
+//            for (int star=1;star<=n-line+1;star++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//
+//        }
+        for (int line = n; line >= 1; line--) {
+            for (int star = 1; star <= line; star++) {
                 System.out.print("* ");
             }
             System.out.println();
-            
         }
     }
+
     public static void main(String[] args) {
         int n;
         Scanner sc = new Scanner(System.in);
---
+++ src/main/java/org/example/Patterns_p1/charPrintWIthoutRipitation.java
@@ -0,0 +1,26 @@
+package org.example.Patterns_p1;
+
+import java.util.Scanner;
+
+public class charPrintWIthoutRipitation {
+    public static void pattern(int n) {
+        char ch = 'A';
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No of Rows: ");
+        n = sc.nextInt();
+        pattern(n);
+    }
+}
+
