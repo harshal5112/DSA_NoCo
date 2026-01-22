e90042a78b573840f137df8691e0d95c259ce454
harshal5112 <harshalrasoda@gmail.com>
2026-01-22 23:15:56 +0530

    ButterflyPattern

---
+++ src/main/java/org/example/Patterns_p1/SolidRhombus.java
@@ -0,0 +1,36 @@
+package org.example.Patterns_p1;
+
+import java.util.Scanner;
+
+public class SolidRhombus {
+
+    public static void printSolidRhombus(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces (shift right)
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print("  ");
+            }
+
+            // Stars (fixed count)
+            for (int star = 1; star <= n; star++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        printSolidRhombus(n);
+
+        sc.close();
+    }
+}
