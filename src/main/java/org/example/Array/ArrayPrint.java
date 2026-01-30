1c22bffe317e30645305154997e75f02490f711f
harshal5112 <harshalrasoda@gmail.com>
2026-01-30 23:14:27 +0530

    Array

---
+++ src/main/java/org/example/Array/ArrayPrint.java
@@ -0,0 +1,11 @@
+package org.example.Array;
+
+public class ArrayPrint {
+    public static void main(String arg[]){
+        int marks[]=new int [5];
+
+        int marks[]={ 3,2,4,4,5,5,3};
+
+
+    }
+}
