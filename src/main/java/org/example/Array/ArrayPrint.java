1740bc0e3de30f7a043bf9b8e5c0619402b51180
harshal5112 <harshalrasoda@gmail.com>
2026-02-06 23:48:01 +0530

    Array

--- src/main/java/org/example/Array/ArrayPrint.java
+++ src/main/java/org/example/Array/ArrayPrint.java
@@ -7,13 +7,20 @@ public class ArrayPrint {
         int marks[] = new int[ 10];
         Scanner sc = new Scanner(System.in);
         int phy;
+
+
         marks[0] = sc.nextInt(); //phy
         marks[1] = sc.nextInt(); //chem
         marks[2] = sc.nextInt(); // math
+
+
         System.out.println("phy :" +  marks[0]);
         System.out.println("chem :" + marks[1]);
         System.out.println("math :" + marks[2]);
+
+
         Float percentage = (float) ((marks[0] + marks[1] + marks[2]) / 3);
         System.out.println("percentage = " + percentage + "%");
+
     }
 }
